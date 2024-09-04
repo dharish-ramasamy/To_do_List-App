62a7757075f0cf8258b68ea787918f279a1be410
dharish-ramasamy <dharishjayakodi@gmail.com>
2024-09-04 14:39:00 +0530

    Updated

--- script.js
+++ script.js
@@ -100,17 +100,14 @@ function getLocalTodos() {
       newTodo.innerText = todo;
       newTodo.classList.add("todo-item");
       todoDiv.appendChild(newTodo);
-
       const completedButton = document.createElement("button");
       completedButton.innerHTML = '<i class="fas fa-check-circle"></li>';
       completedButton.classList.add("complete-btn");
       todoDiv.appendChild(completedButton);
-
       const trashButton = document.createElement("button");
       trashButton.innerHTML = '<i class="fas fa-trash"></li>';
       trashButton.classList.add("trash-btn");
       todoDiv.appendChild(trashButton);
-
       todoList.appendChild(todoDiv);
   });
 }
